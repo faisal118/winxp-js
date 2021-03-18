366e17c82c6dc3fa0d74e06b793a74e5b6d7df20
faisal118 <48615353+faisal118@users.noreply.github.com>
2021-03-18 14:53:37 +0430

    Sounds & Boot Start

--- js/login.js
+++ js/login.js
@@ -1,13 +1,40 @@
+
+const startupAudio = new Audio('./sound/startup.mp3');
+const shutdownAudio = new Audio('./sound/shutdown.mp3');
 function logMeIn() {
     document.querySelector('#xplogo').style.display = 'none';
     document.querySelector('.right-panel').classList.remove('right-panel-active');
-    document.querySelector('#myUser').style.opacity = 1;
+    // document.querySelector('#myUser').style.opacity = 1;
 
     document.querySelector('.welcome').style.display = 'block';
     document.querySelector('.subtext').style.display = 'block';
     document.querySelector('.turnbutton').style.display = 'none';
     document.querySelector('.footertext').style.display = 'none';
+    
+    setTimeout(() => {
+        startupAudio.play();
+    }, 1000);
+    
     setTimeout(() => {
         document.querySelector('.login-screen').style.display = 'none';
-    }, 3000);
+    }, 2000);
+
+}
+
+function logMeOut() {
+    document.querySelector('.login-screen').style.display = 'block';
+    document.querySelector('#xplogo').style.display = 'block';
+    document.querySelector('.right-panel').classList.add('right-panel-active');
+    // document.querySelector('#myUser').style.opacity = 0.5;
+    document.querySelector('.welcome').style.display = 'none';
+    document.querySelector('.subtext').style.display = 'none';
+    document.querySelector('.turnbutton').style.display = 'block';
+    document.querySelector('.footertext').style.display = 'block';
+    
+    shutdownAudio.play();
+    // setTimeout(() => {
+    //     shutdownAudio.play();
+    // }, 1000);
+    
+
 }
