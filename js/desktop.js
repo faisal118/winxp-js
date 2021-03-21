19ecd9bebbebeaf1c6258989340df98bfdb9b952
faisal118 <48615353+faisal118@users.noreply.github.com>
2021-03-21 15:14:38 +0430

    Icons Color Overlay

--- js/desktop.js
+++ js/desktop.js
@@ -2,7 +2,40 @@ class Icon {
     constructor(title,image) {
         this.title = title;
         this.image = image;
+        this.selected = false;
+        this.index = icons.length;
+        this.elem = document.createElement("div");
+        this.elem.classList.add('desktop-icon');
+        this.elem.addEventListener('mousedown',e=>{
+            this.select();
+        });
+        const img = document.createElement("img");
+        img.src='./img/icons/'+this.image;
+        img.width = 32;
+        img.height = 32;
+        this.elem.appendChild(img);
+
+        const text = document.createElement("div");
+        text.classList.add('icon-title');
+        text.innerHTML = this.title;
+        this.elem.appendChild(text);
+
     }
+    select() {
+        this.elem.classList.add('icon-selected');
+        iconsUnSelect(this.index);
+    }
+    unSelect() {
+        this.elem.classList.remove('icon-selected');
+    }
+}
+
+function iconsUnSelect(ind) {
+    icons.forEach(i=>{
+        if (i.index!=ind) {
+            i.unSelect();
+        }
+    });
 }
 
 
@@ -15,21 +48,8 @@ icons.push(new Icon('Recycle Bin',"Icon273.png"));
 function drawIcons() {
     let desktop = document.querySelector('.desktop');
     icons.forEach(i=>{
-        const icon = document.createElement("div");
-        icon.classList.add('desktop-icon');
-
-        const img = document.createElement("img");
-        img.src='./img/icons/'+i.image;
-        img.width = 32;
-        img.height = 32;
-        icon.appendChild(img);
-
-        const text = document.createElement("div");
-        text.classList.add('icon-title');
-        text.innerHTML = i.title;
-        icon.appendChild(text);
-
-        desktop.appendChild(icon);
+        
+        desktop.appendChild(i.elem);
     });
 }
 
